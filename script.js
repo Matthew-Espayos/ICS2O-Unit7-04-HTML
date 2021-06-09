1c70af194e0c766b6726d2f4c1f384bddc9a8c08
MatthewEspayos <84516496+Matthew-Espayos@users.noreply.github.com>
2021-06-09 14:41:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Creating variables
+let age = 0
+let day = ""
+// Connect "Age?" button to "random" function
+document.getElementById('mybutton').addEventListener('click', checkageandday)
+// Get users age and convert it to an integer
+function checkageandday () {
+  age = document.getElementById('myinput').value
+  age = parseInt(age)
+  // Checking to see if they are eligible for student pricing. 
+  if ((day == 'Tuesday' || day == 'Thursday') || (age > 12 && age < 21)) {
+    document.getElementById('myanswer').innerHTML = 'You are eligible for student pricing'
+  } else {
+    document.getElementById('myanswer').innerHTML = 'You are not eligible for student pricing and must pay the regular price'
+  }
+}
